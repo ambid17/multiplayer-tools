932900b5d20fe17131f19ea05c01bd71e2ca320b
Sabien <ambid17@gmail.com>
2025-12-02 16:54:05 -0600

    ParrelSync set up to auto-start server

--- Assets/Scripts/ConnectionHandler.cs
+++ Assets/Scripts/ConnectionHandler.cs
@@ -1,16 +1,52 @@
+using FishNet;
+using FishNet.Transporting;
 using UnityEngine;
 
 public class ConnectionHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+#if UNITY_EDITOR
+        HandleLocalTesting();
+#endif
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+    
+    void HandleLocalTesting()
+    {
+        if(ParrelSync.ClonesManager.IsClone())
+        {
+            InstanceFinder.ClientManager.StartConnection();
+        }
+        else
+        {
+            InstanceFinder.ServerManager.StartConnection();
+            InstanceFinder.ClientManager.StartConnection();
+        }
+    }
+
+#if UNITY_EDITOR
+    private void OnEnable()
+    {
+        InstanceFinder.ClientManager.OnClientConnectionState += OnClientConnectionState;
+    }
+
+    private void OnDisable()
+    {
+        if(InstanceFinder.ClientManager)
+            InstanceFinder.ClientManager.OnClientConnectionState -= OnClientConnectionState;
+    }
+
+    private void OnClientConnectionState(ClientConnectionStateArgs args)
+    {
+        if(args.ConnectionState == LocalConnectionState.Stopping)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+    }
+#endif
 }
